754adffdb2a2aea04124c4f370ecaa6633229d6c
Yossi Rizgan <yosriz@gmail.com>
2018-09-15 23:23:18 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,45 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Environment, Kin } from 'react-native-kin-devplatform';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+
+    console.log("Environment.playground() = " + JSON.stringify(Environment.playground()));
+    Kin.start("some jwt", Environment.playground());
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
